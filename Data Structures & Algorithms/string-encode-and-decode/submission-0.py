8608975625cccd91d9250c2184cf19ddb2f25f23
Akhilesh Kovelakuntla <akhileshkovelakuntla@gmail.com>
2026-08-04 15:04:29 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        encoded_string = ""
+        for s in strs:
+
+            encoded_string += f"{len(s)}#{s}"
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+
+        res = []
+        i = 0
+        
+        while i < len(s):
+            # Find where the delimiter character '#' is
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            
+
+            start_of_str = j + 1
+            end_of_str = start_of_str + length
+            res.append(s[start_of_str:end_of_str])
+            i = end_of_str
+            
+        return res
